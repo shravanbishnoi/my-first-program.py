799df8e682b5ca5f62095d9f49d5536cae0acb23
Shravan Bishnoi <116088301+shravanbishnoi@users.noreply.github.com>
2022-10-22 11:23:05 +0530

    Create firstprogram.py

---
+++ firstprogram.py
@@ -0,0 +1,5 @@
+       #Taking two numbers in float type.
+number_1=float(input("Enter the first number"))
+number_2=float(input("enter the second number"))
+       #Here {0}fulfills value entered in number_1 , {1}fulfills value entered in number_2 and {2} fulfills sum of values entered in number_1 and number_2.
+print(f"The input numbers are {number_1} and {number_2}; Their sum is:{round(number_1+number_2,2)}.")
