084bd1e4c090d15cdaae93f9766e5c2931398632
Shravan Bishnoi <116088301+shravanbishnoi@users.noreply.github.com>
2022-10-22 11:36:47 +0530

    Update firstprogram.py

--- firstprogram.py
+++ firstprogram.py
@@ -1,5 +1,6 @@
        #Taking two numbers in float type.
 number_1=float(input("Enter the first number"))
 number_2=float(input("enter the second number"))
-       #Here {0}fulfills value entered in number_1 , {1}fulfills value entered in number_2 and {2} fulfills sum of values entered in number_1 and number_2.
+       #Here {number_1}fulfills value entered in number_1 , {number_2}fulfills value entered in number_2 and {number_1+number_2} fulfills sum of values entered in number_1 and number_2.
+       #we will get the sum of number_1 and number_2 upto two decimal places as result.
 print(f"The input numbers are {number_1} and {number_2}; Their sum is:{round(number_1+number_2,2)}.")
